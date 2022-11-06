f4985ec0589781b55c917a3dcc2e736d6a28b4d3
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-11-06 07:39:43 +0530

    inspector added

---
+++ models/Inspector.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const inspector = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    nic: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    assigned: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("inspector", inspector);
