e0b7f1cb510c09f87c2c0e2045f66404dbab1a5e
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2022-11-06 07:39:51 +0530

    inspector routes added

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(cors());
 
 connectDB();
 
-app.get("/", (req,res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/user', require('./routes/user.route'));
-app.use('/api/payment', require('./routes/payment.route'));
-app.use('/api/bus', require('./routes/bus.route'));
-app.use('/api/qr', require('./routes/qr.route'));
-app.use('/api/timetable', require('./routes/timetable.route'));
-app.use('/api/report', require('./routes/report.route'));
+app.use("/api/user", require("./routes/user.route"));
+app.use("/api/payment", require("./routes/payment.route"));
+app.use("/api/bus", require("./routes/bus.route"));
+app.use("/api/qr", require("./routes/qr.route"));
+app.use("/api/timetable", require("./routes/timetable.route"));
+app.use("/api/report", require("./routes/report.route"));
+app.use("/api/inspector", require("./routes/inspector.route"));
 
 const PORT = process.env.PORT || 5000;
 
